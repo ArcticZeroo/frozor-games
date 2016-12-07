3ecc32bb869781e79edb73ae1e4462810d884293
ArcticZeroo <spencer@artix.cc>
2016-12-07 12:42:33 -0500

    Add handling for PlayerDamage in DamageManager

--- src/frozor/managers/DamageManager.java
+++ src/frozor/managers/DamageManager.java
@@ -1,18 +1,36 @@
 package frozor.managers;
 
 import frozor.arcade.Arcade;
+import frozor.enums.GameState;
+import frozor.events.GameStateChangeEvent;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DamageManager implements Listener{
     private Arcade arcade;
     private NotificationManager deathNotificationManager = new NotificationManager("Death");
 
+    private boolean allowPlayerDamage = true;
+
     public DamageManager(Arcade arcade){
         this.arcade = arcade;
         this.arcade.RegisterEvents(this);
+    }
+
+    public void setAllowPlayerDamage(boolean allowPlayerDamage) {
+        this.allowPlayerDamage = allowPlayerDamage;
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageEvent event){
+        if(event.getEntityType() != EntityType.PLAYER) return;
+        if(!allowPlayerDamage){
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
@@ -20,5 +38,12 @@ public class DamageManager implements Listener{
         event.setDeathMessage("");
         event.getEntity().setHealth(20);
         arcade.getPlugin().getServer().broadcastMessage(deathNotificationManager.getMessage(event.getEntity().getDisplayName() + ChatColor.GRAY + " killed by " + event.getEntity().getLastDamageCause().getEntity().getName()));
+    }
+
+    @EventHandler
+    public void onGameStateChange(GameStateChangeEvent event){
+        if(event.getGameState() == GameState.START){
+            setAllowPlayerDamage(arcade.getGame().getSettings().isPlayerDamageAllowed());
+        }
     }
 }
