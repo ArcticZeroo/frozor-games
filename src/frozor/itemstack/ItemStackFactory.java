caf006772dc645d95758cf8bf22600c8e0be3e27
Spencer <spencer@artix.cc>
2016-12-06 23:34:57 -0500

    Begninning of ItemStackFactory

---
+++ src/frozor/itemstack/ItemStackFactory.java
@@ -0,0 +1,12 @@
+package frozor.itemstack;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemStackFactory {
+    /*public static ItemStack UnbreakableItem(Material material){
+        ItemStack item = new ItemStack(material);
+
+        item.spigot().setUnbreakable(true);
+    }*/
+}
