a65e1268b27996abcff885994979a10605f30aef
ArcticZeroo <spencer@artix.cc>
2016-12-08 16:04:47 -0500

    Begin work on FrozorScoreboard

---
+++ src/frozor/component/FrozorScoreboard.java
@@ -0,0 +1,44 @@
+package frozor.component;
+
+import frozor.arcade.Arcade;
+import org.bukkit.ChatColor;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
+
+public class FrozorScoreboard {
+    private Arcade arcade;
+    private Scoreboard scoreboard;
+    private Objective objective;
+    private ChatColor[] scoreNames = {ChatColor.AQUA, ChatColor.BLACK, ChatColor.BLUE, ChatColor.DARK_AQUA, ChatColor.DARK_GRAY, ChatColor.DARK_GREEN, ChatColor.DARK_PURPLE, ChatColor.DARK_RED, ChatColor.GOLD, ChatColor.GRAY, ChatColor.GREEN, ChatColor.LIGHT_PURPLE, ChatColor.RED, ChatColor.WHITE, ChatColor.YELLOW, ChatColor.BOLD};
+    private Team[] teams = new Team[16];
+
+    private int nextScoreboardSlot = -1;
+
+    private int getNextScoreboardSlot(){
+        return nextScoreboardSlot--;
+    }
+
+    public FrozorScoreboard(Arcade arcade, String scoreboardName){
+        ScoreboardManager scoreboardManager = arcade.getPlugin().getServer().getScoreboardManager();
+        scoreboard = scoreboardManager.getNewScoreboard();
+        objective = scoreboard.registerNewObjective(scoreboardName, "dummy");
+
+        for(int i = -1; i >= teams.length*-1; i--){
+            teams[i] = scoreboard.registerNewTeam(scoreboardName+"-team-"+i);
+
+            String scoreName = scoreNames[i].toString();
+            teams[i].addEntry(scoreName);
+            objective.getScore(scoreName).setScore(i);
+        }
+    }
+
+    public Scoreboard getScoreboard() {
+        return scoreboard;
+    }
+
+    public void setLine(int index, String text){
+        teams[index].setPrefix(text);
+    }
+}
