0e33a3e49fbe702fd20a68b5d20314068ca7899c
Spencer <spencer@artix.cc>
2016-12-07 22:41:31 -0500

    Update CastleSiege to work with new updates (player min/max, gameWorldName)

--- src/frozor/game/CastleSiege.java
+++ src/frozor/game/CastleSiege.java
@@ -1,11 +1,17 @@
 package frozor.game;
 
 import frozor.kits.*;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 
 public class CastleSiege extends Game implements Listener{
     public CastleSiege(){
-        super("Castle Siege", new PlayerKit[]{new Knight(), new Archer(), new Scout(), new FireMage()});
+        super("Castle Siege", new PlayerKit[]{new Knight(), new Archer(), new Scout(), new FireMage()}, "rivendale");
+
+        getSettings().setPlayerMin(2);
+        getSettings().setPlayerMax(3);
     }
 
     @Override
