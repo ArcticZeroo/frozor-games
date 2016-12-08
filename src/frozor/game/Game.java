bd95d11901a4854acbaa0d684c64e3d26122eb56
Spencer <spencer@artix.cc>
2016-12-07 22:41:06 -0500

    Add mapConfig, spawnLocation, teams, gameWorldName to Game

--- src/frozor/game/Game.java
+++ src/frozor/game/Game.java
@@ -1,26 +1,55 @@
 package frozor.game;
 
 import frozor.arcade.Arcade;
+import frozor.component.DatapointParser;
+import frozor.component.CustomConfig;
 import frozor.kits.PlayerKit;
 import frozor.managers.NotificationManager;
+import frozor.teams.PlayerTeam;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Game extends JavaPlugin{
     protected Arcade arcade;
     protected String title;
     protected PlayerKit[] kits;
+    protected PlayerTeam teams;
     protected NotificationManager notificationManager;
     protected GameSettings settings = new GameSettings();
 
-    public Game(String title, PlayerKit[] kits){
+    protected CustomConfig customConfig = new CustomConfig(this, "map.yml");
+    protected FileConfiguration mapConfig = customConfig.getConfig();
+
+    private String gameWorldName;
+    protected Location spawnLocation;
+
+    public Game(String title, PlayerKit[] kits, String gameWorldName){
         this.title = title;
         this.kits = kits;
 
         notificationManager = new NotificationManager(title);
+
+        //int[] spawnCoords = DatapointParser.toIntArray(mapConfig.getString("spawns.Lobby"));
+
+        //getServer().getWorlds().get(0).setSpawnLocation(spawnCoords[0], spawnCoords[1], spawnCoords[2]);
+
+        this.gameWorldName = gameWorldName;
     }
 
     public void onEnable(){
         arcade = new Arcade(this, kits);
+
+        String lobbySpawn = mapConfig.getString("spawns.Lobby");
+
+        spawnLocation = DatapointParser.parse(lobbySpawn);
+    }
+
+
+    public Location getSpawnLocation() {
+        return spawnLocation;
     }
 
     public NotificationManager getNotificationManager() {
@@ -37,5 +66,9 @@ public class Game extends JavaPlugin{
 
     public GameSettings getSettings() {
         return settings;
+    }
+
+    public FileConfiguration getMapConfig() {
+        return mapConfig;
     }
 }
