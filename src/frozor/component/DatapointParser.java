06b92a86c8cd69e18a055a233828efb27a781c0c
Spencer <spencer@artix.cc>
2016-12-10 16:42:01 -0500

    Yaw and Pitch support in DatapointParser

--- src/frozor/component/DatapointParser.java
+++ src/frozor/component/DatapointParser.java
@@ -6,30 +6,40 @@ import org.bukkit.Location;
 public class DatapointParser {
 
     public static Location parse(String point){
-        double[] coordinates = toDoubleArray(point);
-        return new Location(Bukkit.getWorlds().get(0), coordinates[0], coordinates[1], coordinates[2]);
+        String[] stringCoordinates = toStringArray(point);
+
+        double[] coordinates = toDoubleArray(stringCoordinates);
+
+        Location location = new Location(Bukkit.getWorlds().get(0), coordinates[0], coordinates[1], coordinates[2]);
+
+        location.setPitch(getPitch(stringCoordinates));
+        location.setYaw(getYaw(stringCoordinates));
+
+        return location;
     }
 
     public static String[] toStringArray(String point){
         return point.split(",");
     }
 
-    public static int[] toIntArray(String point){
-        String[] coordinates = toStringArray(point);
-        int x = Integer.parseInt(coordinates[0]);
-        int y = Integer.parseInt(coordinates[1]);
-        int z = Integer.parseInt(coordinates[2]);
-
-        return new int[]{x, y, z};
-    }
-
-    public static double[] toDoubleArray(String point){
-        String[] coordinates = toStringArray(point);
+    public static double[] toDoubleArray(String[] coordinates){
         double x = Double.parseDouble(coordinates[0]);
         double y = Double.parseDouble(coordinates[1]);
         double z = Double.parseDouble(coordinates[2]);
 
         return new double[]{x, y, z};
+    }
+
+    public static float getPitch(String[] coordinates){
+        if(coordinates.length < 4) return 0F;
+
+        return Float.parseFloat(coordinates[3]);
+    }
+
+    public static float getYaw(String[] coordinates){
+        if(coordinates.length < 5) return 0F;
+
+        return Float.parseFloat(coordinates[4]);
     }
 
 }
