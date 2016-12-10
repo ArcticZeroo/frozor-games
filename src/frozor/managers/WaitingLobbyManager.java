24f470ac7776d666a4e4f76a31d8d44bdc0bdd7c
Spencer <spencer@artix.cc>
2016-12-10 15:30:00 -0500

    Make WaitingLobbyManager use Arcade's scoreboard

--- src/frozor/managers/WaitingLobbyManager.java
+++ src/frozor/managers/WaitingLobbyManager.java
@@ -1,10 +1,14 @@
 package frozor.managers;
 
 import frozor.arcade.Arcade;
+import frozor.component.FrozorScoreboard;
 import frozor.enums.GameState;
+import frozor.events.GameStateChangeEvent;
+import frozor.events.WaitingTimeChangeEvent;
 import frozor.tasks.WaitingTimer;
 import frozor.util.UtilPlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -33,15 +37,18 @@ public class WaitingLobbyManager implements Listener{
         arcade.getDebugManager().print(String.format("Player Min: %s, Player Max: %s", getPlayerMin(), getPlayerMax()));
 
         waitingTimer = new WaitingTimer(arcade, minTimer);
-        //waitingLobbyScoreboard = new WaitingLobbyScoreboard(this);
-        //waitingLobbyScoreboard = new FrozorScoreboard(arcade, "lobbyScoreboard");
-        //waitingLobbyScoreboard.getObjective().setDisplayName(ChatColor.AQUA + "Waiting for players...");
-        //waitingLobbyScoreboard.setLine(0, "Line 1 Text");
-        //waitingLobbyScoreboard.setLine(1, "Line 2 Text");
-        //waitingLobbyScoreboard.setLine(2, "Line 3 Text");
-        //waitingLobbyScoreboard.setLine(3, "Line 4 Text");
 
         arcade.getDamageManager().setAllowPlayerDamage(false);
+
+        FrozorScoreboard scoreboard = arcade.getGameScoreboard();
+        scoreboard.setNewSidebarObjective("waitSb");
+        scoreboard.setDisplayName(ChatColor.AQUA + (ChatColor.BOLD + "Waiting for players"));
+        scoreboard.setBlankLine(0);
+        scoreboard.setLine(1, ChatColor.GOLD + (ChatColor.BOLD + "Game"));
+        scoreboard.setLine(2, arcade.getGame().getTitle());
+        scoreboard.setBlankLine(3);
+        scoreboard.setLine(4, ChatColor.GREEN + (ChatColor.BOLD + "Players"));
+        updatePlayerCount();
     }
 
     public int getPlayerMin(){
@@ -60,26 +67,38 @@ public class WaitingLobbyManager implements Listener{
         return arcade;
     }
 
+    public String getOnlineString(){
+        return arcade.getPlugin().getServer().getOnlinePlayers().size() + "/" + getPlayerMax();
+    }
+
+    public void updatePlayerCount(){
+        arcade.getGameScoreboard().setLine(5, getOnlineString());
+    }
+
+    public void setScoreboardWaiting(){
+        arcade.getGameScoreboard().setDisplayName(ChatColor.AQUA + (ChatColor.BOLD + "Waiting for players"));
+    }
+
     @EventHandler
     public void onJoin(PlayerJoinEvent event){
         //Only handling events in waiting lobby, so that things don't break.
         if(arcade.getGameState() != GameState.TIMER && arcade.getGameState() != GameState.LOBBY ) return;
-        if(event.getPlayer() == null){
-            arcade.getDebugManager().print("WaitingLobbyManager PlayerJoinEvent - Player is null");
-            return;
-        }
+        if(event.getPlayer() == null) return;
 
+        //Clean and tp player
         UtilPlayer.cleanPlayer(event.getPlayer());
         event.getPlayer().teleport(arcade.getGame().getSpawnLocation());
+
+        //Scoreboard magic
+        updatePlayerCount();
         event.getPlayer().setScoreboard(arcade.getGameScoreboard().getScoreboard());
 
+        //Deal with timer bull
         int onlineCount = Bukkit.getServer().getOnlinePlayers().size();
 
         if(onlineCount >= getPlayerMin() && arcade.getGameState() == GameState.LOBBY){
             //If there are at least minimum players, and the timer hasn't started yet
             waitingTimer.startTimer();
-            arcade.setGameState(GameState.TIMER);
-
         }else if(onlineCount == getPlayerMax()){
             //If players is max, check if the timer can be shortened and do so if it can
             if(waitingTimer.getTime() > maxTimer){
@@ -87,6 +106,20 @@ public class WaitingLobbyManager implements Listener{
             }
         }else if(onlineCount > getPlayerMax()){
             event.getPlayer().kickPlayer("This game is currently full.");
+        }
+    }
+
+    @EventHandler
+    public void onWaitingTimeChange(WaitingTimeChangeEvent event){
+        arcade.getGameScoreboard().setDisplayName(String.format(ChatColor.AQUA + (ChatColor.BOLD + "Starting in %d seconds"), event.getTime()));
+    }
+
+    @EventHandler
+    public void onGameStateChange(GameStateChangeEvent event){
+        if(event.getGameState() == GameState.LOBBY){
+            setScoreboardWaiting();
+        }else if(event.getGameState() == GameState.TIMER){
+            arcade.getGameScoreboard().setDisplayName(String.format(ChatColor.AQUA + (ChatColor.BOLD + "Starting in %d seconds"), getWaitingTimer().getTime()));
         }
     }
 
