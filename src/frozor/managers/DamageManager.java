26c7adf68f782e9e39a2238913866ec947a2189c
Spencer <spencer@artix.cc>
2016-12-10 16:42:15 -0500

    Prevent FriendlyFire in DamageManager

--- src/frozor/managers/DamageManager.java
+++ src/frozor/managers/DamageManager.java
@@ -3,11 +3,14 @@ package frozor.managers;
 import frozor.arcade.Arcade;
 import frozor.enums.GameState;
 import frozor.events.GameStateChangeEvent;
+import frozor.teams.PlayerTeam;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
@@ -26,7 +29,7 @@ public class DamageManager implements Listener{
         this.allowPlayerDamage = allowPlayerDamage;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerDamage(EntityDamageEvent event){
         if(event.getEntityType() != EntityType.PLAYER) return;
         if(!allowPlayerDamage){
@@ -34,11 +37,21 @@ public class DamageManager implements Listener{
         }
     }
 
+    @EventHandler
+    public void onPvP(EntityDamageByEntityEvent event){
+        if(!(event.getEntity() instanceof Player) || !(event.getDamager() instanceof Player)) return;
+
+        if(arcade.getTeamManager().isFriendlyTeam((Player) event.getEntity(), (Player) event.getDamager())) event.setCancelled(true);
+
+    }
+
     @EventHandler(priority =  EventPriority.HIGHEST)
     public void onPlayerDeath(PlayerDeathEvent event){
-        event.setDeathMessage("");
+        event.setDeathMessage(deathNotificationManager.getMessage(event.getEntity().getDisplayName() + ChatColor.GRAY + " killed by " + event.getEntity().getLastDamageCause().getEntity().getName()));
         event.getEntity().setHealth(20);
-        arcade.getPlugin().getServer().broadcastMessage(deathNotificationManager.getMessage(event.getEntity().getDisplayName() + ChatColor.GRAY + " killed by " + event.getEntity().getLastDamageCause().getEntity().getName()));
+
+        event.getEntity().teleport(arcade.getTeamManager().getTeamSpawn(event.getEntity()));
+        //arcade.getPlugin().getServer().broadcastMessage(deathNotificationManager.getMessage(event.getEntity().getDisplayName() + ChatColor.GRAY + " killed by " + event.getEntity().getLastDamageCause().getEntity().getName()));
     }
 
     @EventHandler
