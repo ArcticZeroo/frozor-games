f0aa11ceb242863038bf17a5dd708b52db27dd87
Spencer <spencer@artix.cc>
2016-12-11 11:22:29 -0500

    Make CastleSiege refill chests, have a refill timer, prevent friendly fire on kings

--- src/frozor/game/CastleSiege/CastleSiege.java
+++ src/frozor/game/CastleSiege/CastleSiege.java
@@ -1,21 +1,24 @@
 package frozor.game.CastleSiege;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import frozor.component.DatapointParser;
 import frozor.enums.GameState;
+import frozor.events.ChestRefillTimeChangeEvent;
 import frozor.events.GameStateChangeEvent;
 import frozor.game.Game;
 import frozor.kits.*;
+import frozor.tasks.ChestRefillTimer;
 import frozor.teams.PlayerTeam;
 import frozor.util.UtilEnt;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
@@ -24,9 +27,11 @@ public class CastleSiege extends Game implements Listener{
     private CastleKing redKing;
     private CastleKing blueKing;
     private CastleKingRegen castleKingRegen;
+    private CastleChestFiller chestFiller;
+    private ChestRefillTimer chestRefillTimer;
 
     public CastleSiege(){
-        super("Castle Siege", new PlayerKit[]{new Knight(), new Archer(), new Scout(), new FireMage()}, new PlayerTeam[]{new PlayerTeam("Blue", ChatColor.BLUE), new PlayerTeam("Red", ChatColor.RED)}, "rivendale");
+        super("Castle Siege", new PlayerKit[]{new Knight(), new Archer(), new Scout(), new FireMage(), new Builder()}, new PlayerTeam[]{new PlayerTeam("Blue", ChatColor.BLUE), new PlayerTeam("Red", ChatColor.RED)}, "rivendale");
 
         getSettings().setPlayerMin(2);
         getSettings().setPlayerMax(3);
@@ -42,15 +47,14 @@ public class CastleSiege extends Game implements Listener{
 
     private void createKings(){
         redKing = new CastleKing(this, "Red", ChatColor.RED);
-
-        redKing.getKing().getLocation().setYaw(180F);
-
         blueKing = new CastleKing(this, "Blue", ChatColor.BLUE);
 
         redKing.getKing().setBaby(false);
         blueKing.getKing().setBaby(false);
         redKing.getKing().setVillager(true);
         blueKing.getKing().setVillager(true);
+
+        redKing.getKing().getLocation().setYaw(180F);
     }
 
     @Override
@@ -67,8 +71,19 @@ public class CastleSiege extends Game implements Listener{
         arcade.getGameScoreboard().setBlankLine(3);
         arcade.getGameScoreboard().setLine(4, ChatColor.BLUE + (ChatColor.BOLD + "Blue King"));
         updateBlueHealth();
+        arcade.getGameScoreboard().setBlankLine(6);
+        arcade.getGameScoreboard().setLine(7, ChatColor.GREEN + (ChatColor.BOLD + "Chest Refill"));
 
         castleKingRegen = new CastleKingRegen(this);
+        chestFiller = new CastleChestFiller(this);
+        chestRefillTimer = new ChestRefillTimer(this.getArcade(), 60*5);
+        chestFiller.fillChests();
+        chestRefillTimer.startTimer();
+
+        updateRefillTimer();
+
+        arcade.getTeamManager().getTeams().get("Red").getScoreboardTeam().addEntry(Integer.toString(redKing.getKing().getEntityId()));
+        arcade.getTeamManager().getTeams().get("Blue").getScoreboardTeam().addEntry(Integer.toString(blueKing.getKing().getEntityId()));
     }
 
     @Override
@@ -91,11 +106,59 @@ public class CastleSiege extends Game implements Listener{
         arcade.getGameScoreboard().setLine(5, String.format("%d Health", (int) Math.floor(blueKing.getKing().getHealth())));
     }
 
+    public void updateRefillTimer(){
+        if(arcade.getGameState() == GameState.END) return;
+        arcade.getDebugManager().print("Updating refill timer");
+        if(chestRefillTimer.getTime() > 60){
+            arcade.getGameScoreboard().setLine(8, String.format("%.1f Minutes", chestRefillTimer.getTime() / 60F));
+        }else{
+            arcade.getGameScoreboard().setLine(8, String.format("%d Seconds", chestRefillTimer.getTime()));
+        }
+    }
+
     @EventHandler
     public void onGameStateChange(GameStateChangeEvent event){
         if(event.getGameState() == GameState.PLAYING){
             arcade.getDebugManager().print("Starting regen timer");
             castleKingRegen.startRegenTimer();
+        }else if(event.getGameState() == GameState.END){
+            castleKingRegen.cancel();
+            chestRefillTimer.cancel();
+        }
+    }
+
+    @EventHandler
+    public void onRefillTimerChange(ChestRefillTimeChangeEvent event){
+        if(arcade.getGameState() == GameState.PLAYING){
+            updateRefillTimer();
+            if(event.getTime() == 0){
+                chestFiller.fillChests();
+                getServer().broadcastMessage(ChatColor.GREEN + (ChatColor.BOLD + "Chests have been refilled!"));
+            }
+        }
+    }
+
+    private Boolean isProtectedLocation(Location location){
+        if(location.getBlock().getType() == Material.CHEST){
+            return true;
+        }
+
+        if(location.distance(redKing.getKing().getLocation()) < 6){
+            return true;
+        }
+
+        if(location.distance(redKing.getKing().getLocation()) < 6){
+            return true;
+        }
+
+        return false;
+    }
+
+    @EventHandler
+    public void onProtectedBlockBreak(BlockBreakEvent event){
+        if(isProtectedLocation(event.getBlock().getLocation())){
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(notificationManager.getError("You can't break blocks there!"));
         }
     }
 
@@ -114,19 +177,25 @@ public class CastleSiege extends Game implements Listener{
         }
     }
 
-    @EventHandler
-    public void onKingDamage(EntityDamageEvent event){
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onKingDamageByPlayer(EntityDamageByEntityEvent event){
         if(event.getEntityType() == EntityType.ZOMBIE ){
             if(arcade.getGameState() != GameState.PLAYING){
                 event.setCancelled(true);
                 return;
             }
 
-            if(event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK){
+            if(event.getDamager() instanceof Player){
                 event.setCancelled(true);
+
+                Player player = (Player) event.getDamager();
+                PlayerTeam playerTeam =  arcade.getTeamManager().getPlayerTeam(player);
+
+                if(playerTeam.getScoreboardTeam().hasEntry(Integer.toString(event.getEntity().getEntityId()))) return;
+
                 LivingEntity king = (LivingEntity) event.getEntity();
                 king.damage(1D);
-            }else if(event.getCause() == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION){
+            }else{
                 event.setCancelled(true);
                 return;
             }
@@ -139,6 +208,21 @@ public class CastleSiege extends Game implements Listener{
                 if(blueKing.getKing().getHealth() < 1) return;
                 blueKing.setLastDamage();
                 updateBlueHealth();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onKingDamage(EntityDamageEvent event){
+        if(event.getEntityType() == EntityType.ZOMBIE){
+            if((event.getCause() == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) || (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) || (event.getCause() == EntityDamageEvent.DamageCause.FIRE)){
+                event.setCancelled(true);
+
+                Entity squid = event.getEntity().getPassenger();
+                Entity stand = squid.getPassenger();
+
+                squid.remove();
+                stand.remove();
             }
         }
     }
