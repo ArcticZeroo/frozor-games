03e0dd9d2b0957c019a3824941be9297808281d0
Spencer <spencer@artix.cc>
2016-12-11 11:19:39 -0500

    Add methods to get kingType and kingColor in CastleKing

--- src/frozor/game/CastleSiege/CastleKing.java
+++ src/frozor/game/CastleSiege/CastleKing.java
@@ -1,13 +1,16 @@
 package frozor.game.CastleSiege;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import frozor.component.DatapointParser;
 import frozor.kits.ArmorSet;
 import frozor.kits.ArmorSetType;
+import frozor.teams.PlayerTeam;
 import frozor.util.UtilEnt;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,13 +18,15 @@ public class CastleKing {
     private CastleSiege castleSiege;
     private Zombie king;
     private Long lastDamage = (long) 0;
+    private String kingType;
+    private ChatColor kingColor;
 
     private void giveKingEquipment(Zombie king){
         king.getEquipment().setArmorContents(ArmorSet.getArmorSet(ArmorSetType.IRON));
         king.getEquipment().setHelmet(new ItemStack(Material.GOLD_HELMET));
     }
 
-    private Zombie createKing(String kingType, ChatColor kingColor){
+    private Zombie createKing(){
         Location kingLocation = DatapointParser.parse(castleSiege.getMapConfig().getString("kings."+kingType));
 
         Zombie king = (Zombie) UtilEnt.spawnNamedEntity(kingLocation, EntityType.ZOMBIE, kingColor + (ChatColor.BOLD + kingType + " King"));
@@ -35,7 +40,11 @@ public class CastleKing {
 
     public CastleKing(CastleSiege castleSiege, String kingType, ChatColor kingColor){
         this.castleSiege = castleSiege;
-        this.king = createKing(kingType, kingColor);
+
+        this.kingType = kingType;
+        this.kingColor = kingColor;
+
+        this.king = createKing();
     }
 
     public Zombie getKing() {
