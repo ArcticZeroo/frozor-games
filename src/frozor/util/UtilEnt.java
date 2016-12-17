3d6a8ec3000d9d8854b6b5e8689053436d00fa3a
Spencer <spencer@artix.cc>
2016-12-17 18:16:57 -0500

    Load the chunk when a named entity is spawned, make squid a silverfish.

--- src/frozor/util/UtilEnt.java
+++ src/frozor/util/UtilEnt.java
@@ -1,20 +1,16 @@
 package frozor.util;
 
 import org.bukkit.Location;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Squid;
+import org.bukkit.entity.*;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class UtilEnt {
     public static Entity spawnNamedEntity(Location location, EntityType entityType, String name){
-        System.out.println("Spawning a named entity - " + name);
-        System.out.println("Spawning the entity");
+        location.getWorld().loadChunk(location.getWorld().getChunkAt(location));
+
         Entity entity = location.getWorld().spawnEntity(location, entityType);
 
-        System.out.println("Spawning the armor stand");
         ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(location.add(0, 2, 0), EntityType.ARMOR_STAND);
 
         armorStand.setVisible(false);
@@ -26,14 +22,14 @@ public class UtilEnt {
         armorStand.setMarker(true);
         armorStand.setSmall(true);
 
-        /*Squid squid = (Squid) location.getWorld().spawnEntity(location.add(0, 2, 0), EntityType.SQUID);
-        squid.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 999999, 0), true);
-        squid.setPassenger(armorStand);
+        Silverfish silverfish = (Silverfish)  vegetate(location.getWorld().spawnEntity(location.add(0, 2, 0), EntityType.SILVERFISH));
 
-        entity.setPassenger(squid);*/
-        entity.setPassenger(armorStand);
+        silverfish.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 999999, 0), true);
+        silverfish.setPassenger(armorStand);
 
-        System.out.println("yay");
+        entity.setPassenger(silverfish);
+        //entity.setPassenger(armorStand);
+
         return entity;
     }
 
