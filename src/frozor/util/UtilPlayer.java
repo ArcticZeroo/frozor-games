2c90c83666c7a72e24861df82262b9f9e4624abd
Spencer <spencer@artix.cc>
2016-12-16 23:27:47 -0500

    Reset velocity in CleanPlayer and set FireTicks to 0

--- src/frozor/util/UtilPlayer.java
+++ src/frozor/util/UtilPlayer.java
@@ -2,20 +2,23 @@ package frozor.util;
 
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.util.Vector;
 
 import java.util.Collection;
 
 public class UtilPlayer {
-    public static void cleanPlayer(Player player){
-        player.setGameMode(GameMode.ADVENTURE);
+    public static void cleanPlayer(Player player, GameMode gameMode){
+        player.setGameMode(gameMode);
         player.setHealth(20D);
         player.setFoodLevel(20);
         player.setSaturation(1.0F);
         player.getInventory().clear();
         player.setExp(0F);
         player.setLevel(0);
+        player.setFireTicks(0);
+
+        player.setVelocity(new Vector(0,0,0));
 
         player.getInventory().setHelmet(null);
         player.getInventory().setChestplate(null);
@@ -28,5 +31,9 @@ public class UtilPlayer {
                 player.removePotionEffect(effect.getType());
             }
         }
+    }
+
+    public static void cleanPlayer(Player player){
+        cleanPlayer(player, GameMode.ADVENTURE);
     }
 }
