febe5f74d6a4b7fe3d76bad3201b64393b984420
Spencer <spencer@artix.cc>
2016-12-16 23:28:29 -0500

    Add isSword method to UtilItem

--- src/frozor/util/UtilItem.java
+++ src/frozor/util/UtilItem.java
@@ -1,11 +1,15 @@
 package frozor.util;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class UtilItem {
     public static void ColorLeather(ItemStack itemStack, int red, int blue, int green){
@@ -27,5 +31,16 @@ public class UtilItem {
         itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(itemMeta);
         return item;
+    }
+
+    public static Boolean isSword(ItemStack item){
+        return (item.getType() == Material.WOOD_SWORD || item.getType() == Material.STONE_SWORD || item.getType() == Material.GOLD_SWORD || item.getType() == Material.IRON_SWORD || item.getType() == Material.DIAMOND_SWORD);
+    }
+
+    public static ItemStack setSoulbound(ItemStack itemStack){
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setLore(new ArrayList<>(Arrays.asList(new String[]{ChatColor.BLUE+"Soulbound"})));
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
     }
 }
